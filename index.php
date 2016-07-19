88caaea3a663a1eba1929030027594bfcdfbc9ab
Becky Reamy <becky@abgames.com>
2016-07-18 22:29:35 -0400

    OES-5: Make the view print out all of the properties of the model

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require_once("EventModel.php");
 require_once("EventView.php");
 require_once("EventController.php");
 
-$model = new EventModel();
+$model = new EventModel("Event Name", "Description", "Category", "Start Time", "End Time");
 $controller = new EventController($model);
 $view = new EventView($controller, $model);
 echo $view->output();
