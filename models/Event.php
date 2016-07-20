cd68ad8674ef4d0388fb9a6996bb68f88179502b
Becky Reamy <becky@abgames.com>
2016-07-19 21:06:36 -0400

    OES-5: Organizing the code

---
+++ models/Event.php
@@ -0,0 +1,56 @@
+<?php
+class Events {
+	private $eventName;
+	private $description;
+	private $category;
+	private $startTime;
+	private $endTime;
+
+	public function __construct($eventName, $description, $category, $startTime, $endTime) {
+		$this->eventName = $eventName;
+		$this->description = $description;
+		$this->category = $category;
+		$this->startTime = $startTime;
+		$this->endTime = $endTime;
+	}
+
+	public function setEventName($eventName) {
+		$this->eventName = $eventName;
+	}
+
+	public function getEventName() {
+		return $this->eventName;
+	}
+
+	public function setDescription($description) {
+		$this->description = $description;
+	}
+
+	public function getDescription() {
+		return $this->description;
+	}
+
+	public function setCategory($category) {
+		$this->category = $category;
+	}
+
+	public function getCategory() {
+		return $this->category;
+	}
+
+	public function setStartTime($startTime) {
+		$this->startTime = $startTime;
+	}
+
+	public function getStartTime() {
+		return $this->startTime;
+	}
+
+	public function setEndTime($endTime) {
+		$this->endTime = $endTime;
+	}
+
+	public function getEndTime() {
+		return $this->endTime;
+	}
+}
