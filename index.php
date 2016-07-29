98e72d235ffd9f65476ae79cbf5fab4f4de7abd4
Becky Reamy <becky@abgames.com>
2016-07-28 20:28:09 -0400

    OES-5: Remove unused files

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php
-require_once("models/Event.php");
-require_once("EventView.php");
 require_once("controllers/EventController.php");
 
-$model = new EventModel("Event Name", "Description", "Category", "Start Time", "End Time");
-$controller = new EventController($model);
-$view = new EventView($controller, $model);
-echo $view->output();
+$controller = new EventController();
+echo $controller->getFullEventList();
