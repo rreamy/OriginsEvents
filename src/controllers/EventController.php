57ad28139cc560732cfa224a4807b452532db67a
Becky Reamy <becky@abgames.com>
2016-07-28 21:46:55 -0400

    OES-5: change to work on site

--- src/controllers/EventController.php
+++ src/controllers/EventController.php
@@ -1,5 +1,5 @@
 <?php
-include_once('../dao/EventDao.php');
+include_once(__DIR__.'/../dao/EventDao.php');
 
 class EventController {
 	
--- src/dao/EventDao.php
+++ src/dao/EventDao.php
@@ -1,5 +1,5 @@
 <?php
-include_once('../models/Event.php');
+include_once(__DIR__.'/../models/Event.php');
 
 class EventDao {
 	
